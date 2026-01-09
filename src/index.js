73db3dd61c05c2afd044a0c2a0babe1024fb7a6a
Anant003 <anantpanchal39@gmail.com>
2026-01-09 22:25:04 +0530

    update index component

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { dataManager } from './utils/dataManager';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+// Pre-load critical data at app startup (single batch of 3 API calls)
+dataManager.preloadData().finally(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
